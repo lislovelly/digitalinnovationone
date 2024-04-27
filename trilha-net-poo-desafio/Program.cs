2a97449e6e7471cab84ebe538e2351d518fc40dc
lislovelly <lislovelly@gmail.com>
2024-04-27 08:43:19 -0300

    app

--- trilha-net-poo-desafio/Program.cs
+++ trilha-net-poo-desafio/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine("Smartphone Nokia");
 Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
 nokia.Ligar();
-nokia.InstalarAplicativo("whatsapp");
+nokia.InstalarAplicativo("Whatsapp");
 
 Console.WriteLine("\n");
 

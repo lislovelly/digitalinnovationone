f5dbf5559c7c56cac0e87ff37cdeb716744f3ba7
lislovelly <lislovelly@gmail.com>
2024-05-03 07:24:27 -0300

    validacoes

--- trilha-net-teste/CalculadoraTestes/obj/Debug/net8.0/CalculadoraTestes.AssemblyInfo.cs
+++ trilha-net-teste/CalculadoraTestes/obj/Debug/net8.0/CalculadoraTestes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraTestes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d8624cacda8e9d1ab73800d88bcabc8fe2f33b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61acbbbefb437007ef463d57802ec8c6cc54b669")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraTestes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraTestes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
